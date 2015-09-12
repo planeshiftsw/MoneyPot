e325dc52881f0bd01ea613847576799d1f8cc215
planeshiftsw <chrisandanaikadevs@gmail.com>
2015-09-12 14:45:25 -0600

    Initial skeleton

---
+++ MoneyPot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MoneyPot
+//
+//  Created by Christopher Montoya on 9/11/15.
+//  Copyright © 2015 Christopher Montoya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
